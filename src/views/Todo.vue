c456aec89e85f94f12523a7099993e71978abf4c
Izaz Khan <izazkhan134@gmail.com>
2023-05-01 22:47:44 +0500

    design almost done!

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,39 +1,55 @@
 <template>
-  <div>
-    <v-form>
-      <v-container class="ma-0 pa-0">
-        <v-row>
-          <v-col cols="12" class="pa-0 ma-0">
-            <v-text-field hide-details outlined class="pa-3" label="Add Task" append-icon="mdi-plus"></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-    <v-list flat class="pt-0">
-      <v-list-item-group multiple>
-        <div v-for="task in tasks" :key="task.id">
-          <v-list-item @click="doneTask(task.id)" :class="{ 'blue lighten-5': task.done }">
-            <template v-slot:default>
-              <v-list-item-action>
-                <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
-              </v-list-item-action>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+          <v-row>
+            <v-col cols="12" class="">
+              <v-text-field
+                v-model="newTask" 
+                hide-details 
+                outlined
+                @click:append="addTask()"
+                @keyup.enter="addTask()"
+                label="Add Task"
+                append-icon="mdi-plus"></v-text-field>
+            </v-col>
+          </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-list flat class="pt-0">
+          <v-list-item-group multiple>
+            <div v-for="task in tasks" :key="task.id">
+              <v-list-item class="pa-3" @click="doneTask(task.id)" :class="{ 'blue lighten-5': task.done }">
+                <template v-slot:default>
+                  <v-list-item-action>
+                    <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+                  </v-list-item-action>
 
-              <v-list-item-content>
-                <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{ task.title
-                }}</v-list-item-title>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-btn @click.stop="deleteTodo(task.id)" icon>
-                  <v-icon>mdi-delete</v-icon>
-                </v-btn>
-              </v-list-item-action>
-            </template>
-          </v-list-item>
-          <v-divider></v-divider>
-        </div>
-      </v-list-item-group>
-    </v-list>
-  </div>
+                  <v-list-item-content>
+                    <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{ task.title
+                    }}</v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-action class="m-0">
+                    <v-btn @click.stop="editTodo(task.id)" icon>
+                      <v-icon>mdi-note-edit-outline</v-icon>
+                    </v-btn>
+                  </v-list-item-action>
+                  <v-list-item-action class="ma-0">
+                    <v-btn @click.stop="deleteTodo(task.id)" icon>
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </v-list-item-action>
+                </template>
+              </v-list-item>
+              <v-divider></v-divider>
+            </div>
+          </v-list-item-group>
+        </v-list>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -45,6 +61,7 @@ export default {
 
   data() {
     return {
+      newTask: '',
       tasks: [
         {
           id: 1,
@@ -71,9 +88,17 @@ export default {
       task.done = !task.done;
     },
 
+    addTask() {
+      alert(this.newTask);
+    },
+
     deleteTodo(id) {
       // Call Api to delete task from todo's list
       alert("Delete Task")
+    },
+    
+    editTodo(id) {
+      alert("Edit Task")
     }
   }
 }
